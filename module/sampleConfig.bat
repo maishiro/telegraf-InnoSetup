6c5a6e24a7d4f953317ee436b72dfcdb359a0bbc
maishiro <maishiro2007-engineer@yahoo.co.jp>
2021-01-24 05:58:07 +0900

    個別インストールに変更
    インストールフォルダの変更に対応
    （設定ファイルへフォルダ変更の反映）

--- module/sampleConfig.bat
+++ module/sampleConfig.bat
@@ -1,5 +1,5 @@
 cd /d %~dp0
 
-telegraf --sample-config --input-filter exec     --output-filter file >telegraf-1.conf
-telegraf --sample-config --input-filter fileexec --output-filter file >telegraf-2.conf
-telegraf --sample-config --input-filter win_perf_counters --output-filter influxdb >telegraf-WinHost.conf
+telegraf --sample-config --input-filter exec     --output-filter file --processor-filter none --aggregator-filter none >telegraf-1.conf
+telegraf --sample-config --input-filter fileexec --output-filter file --processor-filter none --aggregator-filter none >telegraf-2.conf
+telegraf --sample-config --input-filter win_perf_counters --output-filter influxdb --processor-filter none --aggregator-filter none >telegraf-WinHost.conf
